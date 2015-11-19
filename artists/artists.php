87d530d00cab12075a302e95594075c196bb8fce
Aleksandr Antonov <antonovfineart@gmail.com>
2015-11-19 14:51:25 -0700

    Created Basic File tree and initialized .htaccess file

---
+++ artists/artists.php
@@ -0,0 +1,16 @@
+<?php 
+$page = "artists";
+$title = "Sloane Gallery | ".ucfirst($page);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $title ?></title>
+<link rel="stylesheet" type="text/css" href="/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="/css/general.css">
+</head>
+<body>
+
+
+</body>
+</html>
