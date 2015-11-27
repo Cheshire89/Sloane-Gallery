9fa5eb9324f0fcc80d9183bf8ac1a151f90095a0
Aleksandr Antonov <antonovfineart@gmail.com>
2015-11-26 19:59:58 -0700

    updated all file paths to use BASE_URL. Added conditionals to change styling of the active link

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,23 +1,30 @@
 <?php 
 $gallery = "Sloane Gallery";
+include("config.php");
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
-  <title><?php echo $gallery." | ".ucfirst($page) ?></title>
-  <link href="/stylesheets/normalize.css" rel="stylesheet" type="text/css" />
+  <title><?php 
+  if(isset($page) == TRUE){
+    echo $gallery." | ".ucfirst($page);
+  } else {
+    echo $gallery;
+  } 
+  ?></title>
+  <link href="<?php echo BASE_URL; ?>stylesheets/normalize.css" rel="stylesheet" type="text/css" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <link href="/stylesheets/defult.css.css" rel="stylesheet" type="text/css" />
+  <link href="<?php echo BASE_URL; ?>stylesheets/defult.css.css" rel="stylesheet" type="text/css" />
 <?php 
       if($page == "index" || $page == "artists" || $page == "shows" 
         || $page == "contact" || $page == "about"){
-        echo '<link href="/stylesheets/'.$page.'.css" rel="stylesheet" type="text/css" />'; // index style file
+        echo '<link href="'.BASE_URL.'stylesheets/'.$page.'.css" rel="stylesheet" type="text/css" />'; // index style file
       } 
 
        echo '<style type="text/css">
           html { 
-            background: url(/images/bg'.rand(1,4).'.jpg) no-repeat center center fixed; 
+            background: url('.BASE_URL.'images/bg'.rand(1,4).'.jpg) no-repeat center center fixed; 
             background-color: #d6d6d6;
             -webkit-background-size: cover;
             -moz-background-size: cover;
@@ -27,26 +34,28 @@ $gallery = "Sloane Gallery";
         </style>';
 
 ?>
-  <script type="text/javascript" src="scripts/browser-compatibility.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_URL; ?>scripts/browser-compatibility.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script type="text/javascript" src="/scripts/sticky.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_URL; ?>scripts/sticky.js"></script>
   
  </head>
  <body>
   <header class="header">
    <div class="headerContainer gridContainer">
     <div class="logo logo_align">
-     <img class="image" alt="Sloane Logo" src="images/Sloane_Logo.png" />
+     <img class="image" alt="Sloane Logo" src="<?php echo BASE_URL; ?>images/Sloane_Logo.png" />
     </div>
     <div class="menu_unit_wrapper">
      <div class="menu menu_align">
       <nav class="clearfix">
        <ul class="hmenubar1_list">
-        <li class="item"><a class="clicked" href="#" target="_self">Artists</a></li>
-        <li><a href="#" target="_self">Shows</a></li>
-        <li><a href="#">Publications</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#" target="_self">Contact<br/></a></li>
+        
+        <li <?php if ($page == "artists"){echo'class="item"';} ?>><a <?php if ($page == "artists"){echo'class="clicked"';} ?> href="<?php echo BASE_URL;?>artists/">Artists</a></li>
+        <li <?php if ($page == "shows"){echo'class="item"';} ?>><a <?php if ($page == "shows"){echo'class="clicked"';} ?> href="<?php echo BASE_URL;?>shows/">Shows</a></li>
+        <li <?php if ($page == "publications"){echo'class="item"';} ?>><a <?php if ($page == "publications"){echo'class="clicked"';} ?> href="<?php echo BASE_URL;?>publications/">Publications</a></li>
+        <li <?php if ($page == "about"){echo'class="item"';} ?>><a <?php if ($page == "about"){echo'class="clicked"';} ?> href="<?php echo BASE_URL;?>about/">About</a></li>
+        <li <?php if ($page == "contact"){echo'class="item"';} ?>><a <?php if ($page == "contact"){echo'class="clicked"';} ?> href="<?php echo BASE_URL;?>contact/">Contact</a></li>
+        
        </ul>
        <select>
         <option value="" selected="">MENU</option>
@@ -65,7 +74,7 @@ $gallery = "Sloane Gallery";
       <option value="#">About</option>
       <option value="#">Contact</option>
      </select>
-     <script type="text/javascript" src="scripts/mobile_menu_selection.js"></script>
+     <script type="text/javascript" src="<?php echo BASE_URL; ?>scripts/mobile_menu_selection.js"></script>
     </div>
   
    </div>
