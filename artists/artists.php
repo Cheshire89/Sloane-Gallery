c6c8ff411e977efb822b8ac5464dd5cffc7e0511
Aleksandr Antonov <antonovfineart@gmail.com>
2015-11-27 13:30:10 -0700

    Replaced gallery_db.sql with myPhpAdmin.  Connected artists.php to connect.php initialised artists list with links to artist.php page

--- artists/artists.php
+++ artists/artists.php
@@ -2,11 +2,27 @@
 $page = "artists";
 require_once($_SERVER["DOCUMENT_ROOT"]."/php/includes/config.php");
 include(ROOT_PATH.'php/includes/header.php');
+include('connect.php');
+
+try{
+	$results = $db -> query('select * from artists order by artist_id');
+} catch(Exception $e){
+	echo $e -> getMessage();
+	die();
+}
+
+$artists = $results -> fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <section class="body">
    <div class="bodyContainer">
-      
-      
+      <ul>
+	      <?php 
+	      		foreach($artists as $artist){
+	      		echo '<li><a href="artist.php? id='.$artist["artist_id"].'">'.$artist["artist_name"].'</a></li>'; 
+	      		}
+	      ?>
+      </ul>
    </div>
 </section>
 
