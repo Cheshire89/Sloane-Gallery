810d4163a55e4e5e051c4c2c31c6e86633b5ae7e
Aleksandr Antonov <antonovfineart@gmail.com>
2015-11-26 20:04:37 -0700

    created directories for the main pages

---
+++ publications/publications.php
@@ -0,0 +1,15 @@
+<?php
+$page = "publications";
+require_once($_SERVER["DOCUMENT_ROOT"]."/php/includes/config.php");
+include(ROOT_PATH.'php/includes/header.php');
+?>
+<section class="body">
+   <div class="bodyContainer">
+      
+      
+   </div>
+</section>
+
+<?php
+include(ROOT_PATH.'php/includes/footer.php');
+?>
