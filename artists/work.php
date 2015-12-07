13a87dff1115d6cca6ab927697ed78a09ae9e70c
Aleksandr Antonov <antonovfineart@gmail.com>
2015-12-07 15:45:44 -0700

    Removed old code and substituted it with a function to include art_works from database

--- artists/work.php
+++ artists/work.php
@@ -1,29 +1,10 @@
 <?php
 $page = "work";
 require_once($_SERVER["DOCUMENT_ROOT"]."/php/includes/config.php");
-include('connect.php');
+include('functionList.php');
 
-if(!empty($_GET['id'])){
-	
-   $work_id = intval($_GET['id']);
+$artist = loadArtistWorks('select * from art_works where artist = ?');
 
-	 try{
-    $results = $db -> prepare('select * from art_works where work_id = ?');
-    $results -> bindParam(1,$work_id); //#1 references the "?" and binds a variable to that "?"
-    $results -> execute();
-    
-    } catch (Exception $e){
-    
-    echo $e->getMessage();
-    die();
-    }
-
-    $work = $results -> fetchAll(PDO::FETCH_ASSOC); //In this case it fetches just a single row;
-    if($work == FALSE){
-      echo "Sorry no artist was found with the provided Id";
-    }
-
-}
 include(ROOT_PATH.'php/includes/header.php');
 ?>
 <section class="body">
