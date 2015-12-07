3652eb2ab1acb171637c64c458ed034321278211
Aleksandr Antonov <antonovfineart@gmail.com>
2015-12-07 00:38:44 -0700

    Added the images for the mouse_over, the javascript code to run it, and made adjustments to artists.php

--- artists/artists.php
+++ artists/artists.php
@@ -2,27 +2,38 @@
 $page = "artists";
 require_once($_SERVER["DOCUMENT_ROOT"]."/php/includes/config.php");
 include(ROOT_PATH.'php/includes/header.php');
-include('connect.php');
+include('functionList.php');
 
-try{
-	$results = $db -> query('select * from artists order by artist_id');
-} catch(Exception $e){
-	echo $e -> getMessage();
-	die();
-}
-
-$artists = $results -> fetchAll(PDO::FETCH_ASSOC);
+$artists = loadListOfArtists();
 
 ?>
 <section class="body">
    <div class="bodyContainer">
+      <div class="imgContainer">
+       <?php
+               
+        ?>
+         <a href="<?php echo $link; ?>">
+            <img src="img/artistStart.jpg" name="targetimage">
+         </a>
+         <div class="caption">
+            <p>
+               <?php
+              
+               ?>
+            </p>
+         </div>
+      </div>
+      <div class="artistsList">
       <ul class="list">
 	      <?php 
       		foreach($artists as $artist){
-      		echo '<li><a href="artist.php?id='.$artist["artist_id"].'">'.$artist["artist_name"].'</a></li>'; 
+      		echo '<li><a onMouseover="changeimage(myimages['.$artist["artist_id"].'],this.href)" 
+            href="artist.php?id='.$artist["artist_id"].'" id="target_'.$artist["artist_id"].'">'.$artist["artist_name"].'</a></li>'; 
       		}
 	      ?>
       </ul>
+      </div>
    </div>
 </section>
 
