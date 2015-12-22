36bd39cf37758bf876434ae1cd2601c7c121d306
Aleksandr Antonov <antonovfineart@gmail.com>
2015-12-22 11:55:38 -0700

    Replased the date in the footer with dynamic function

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -1,6 +1,6 @@
  <footer class="footer">
    <div class="footerContainer">
-    	<h2 class="heading"> Sloane Gallery of Art | 1777 Larimer St Unit 102B Denver, CO 80202 | (303)595 4230 | ©2015 </h2>
+    	<h2 class="heading"> Sloane Gallery of Art | 1777 Larimer St Unit 102B Denver, CO 80202 | (303)595 4230 | <?php echo "&copy;".date("Y"); ?> </h2>
 
    </div>
   </footer>
