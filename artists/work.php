b8294fc8715bd56a744d7ef2b55e3d0e9f885def
Aleksandr Antonov <antonovfineart@gmail.com>
2016-02-05 12:41:18 -0700

    added a function to convert string with inches to string in cm

--- artists/work.php
+++ artists/work.php
@@ -6,18 +6,32 @@ $artWork = selectParamQuery('select * from art_works where work_id = ?');
 $artist = getArtistId();
 $artistName = selectParamQuery('select * from artists where artist_id ='.$artist);
 $title = $artistName[0]['artist_name'];
+$artwork = $_GET["workId"];
+
+function valueToCm($value){
+	$newValue = '';
+	$value = explode('x', $value);
+
+	for($i=0; $i<count($value);$i++){
+		$value[$i] = floatval(number_format(($value[$i] * 2.54),1));
+		$newValue .= $value[$i].' ';
+	}
+
+	return $newValue = str_replace(' ',' x ', trim($newValue)).' cm';
+}
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/SloaneGallery/php/includes/config.php");
 include(ROOT_PATH.'php/includes/header.php');
 ?>
 <section class="body">
    <div class="bodyContainer">
+
    	<div id="imageHolder">
    		<div id="aLeft" class="imageBlock">
    			<img src="img/arrowL.png">
    		</div>
    		<div id="img" class="imageBlock">
-   			<img src="img/L_placeholder.jpg" alt="Placeholder">
+   			<img src="img/L_placeholder.jpg" alt="Placeholder">		
    		</div>
    		<div id="aRight"class="imageBlock">
    			<img src="img/arrowR.png">
@@ -25,11 +39,22 @@ include(ROOT_PATH.'php/includes/header.php');
    	</div>
    	<div id="infoHolder">
    		<ul>
-   			<li><strong>Title:</strong>Thing One</li>
-   			<li><strong>Title:</strong>Thing Two</li>
-   			<li><strong>Title:</strong>Thing Three Longer String Test</li>
-   			<li><strong>Title:</strong>Thing Four</li>
-   			<li><strong>Title:</strong>Thing Five</li>
+   		<?php
+   			foreach ($artWork as $work){
+   				foreach ($work as $key => $value){
+   					if($key != "work_id" && $key != "artist" && $value != ""){
+   						if($key === "yearOfcreation"){
+   							echo '<li> Year:  '.$value.'</li>';
+   						} elseif ($key === "size"){	
+   							echo '<li>'.ucfirst($key).':  '.$value .' in  &nbsp;&nbsp;('.valueToCm($value).')</li>';
+   							
+   						} else {
+   							echo '<li>'.ucfirst($key).':  '.$value.'</li>';
+   						}
+   					}
+   				}
+   			}
+   		?>
    		</ul>
    		
    	</div>
