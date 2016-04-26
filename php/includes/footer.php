24b15818067d171b1f3cc47c8042828bcd73e016
Aleksandr Antonov <antonovfineart@gmail.com>
2016-04-26 14:55:22 -0600

    Optimisation. Got rid of bootstrap for now. Ran all images through smush.it removed unnecessare placeholder images

--- php/includes/footer.php
+++ php/includes/footer.php
@@ -9,7 +9,7 @@
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
   <script type="text/javascript" src="<?php echo BASE_URL; ?>scripts/sticky.js"></script>
   <script type="text/javascript" src="<?php echo BASE_URL; ?>scripts/mobile_menu_selection.js"></script>
-  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <!--<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
 <?php 
   if($page === "artists"){
    echo '<script type="text/javascript" src="'.BASE_URL.'scripts/mouse_over.js"></script>';
