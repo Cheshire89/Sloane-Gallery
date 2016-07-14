3eefd637f095766b9b35723cfea2d88c33b3fa59
Aleksandr Antonov <antonovfineart@gmail.com>
2016-07-14 15:16:56 -0600

    Added 3 works to Tengiz. Marked some as sold and removed artists if there were no works left.

--- php/ignoreList.php
+++ php/ignoreList.php
@@ -1,5 +1,5 @@
 <?php
-	$ignoreListArtists = array(1,36,53);
+	$ignoreListArtists = array(1,3,24,30,36,38,53);
 	$ignoreListArtWorks = array(40,109,158,159,278,280);
 	$ukranians = array(3,15,17,30,38,51);
 	$change_order = array(16,27,32,54,61);
--- php/listOfportraits.php
+++ php/listOfportraits.php
@@ -23,70 +23,70 @@ $artists = array(
 		"title" => "Ernst Neizvestny",
 		"img" => IMG_PATH."Ernst Neizvestny.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Ernst-Neizvestny"
+		"link" => BASE_URL."artists/Ernst_Neizvestny"
 		),
 	$bulatov = array(
 		"title" => "Erik Boulatov",
 		"img" => IMG_PATH."Erik Boulatov.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Erik-Boulatov"
+		"link" => BASE_URL."artists/Erik_Boulatov"
 		),
 	$yelchin = array(
 		"title" => "Eugene Yelchin",
 		"img" => IMG_PATH."Eugene Yelchin.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Eugene-Yelchin"
+		"link" => BASE_URL."artists/Eugene_Yelchin"
 		),
 	$chef = array(
 		"title" => "Genia Chef",
 		"img" => IMG_PATH."Genia Chef.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Genia-Chef"
+		"link" => BASE_URL."artists/Genia_Chef"
 		),
 	$kabakov = array(
 		"title" => "Ilya Kabakov",
 		"img" => IMG_PATH."Ilya Kabakov.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Ilya-Kabakov"
+		"link" => BASE_URL."artists/Ilya_Kabakov"
 		),
 	$km = array(
 		"title" => "Komar & Melamid",
 		"img" => IMG_PATH."Komar And Melamid.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Komar-Melamid"
+		"link" => BASE_URL."artists/Komar_Melamid"
 		),
 	$kosolapov = array(
 		"title" => "Alexander Kosolapov",
 		"img" => IMG_PATH."Alexander Kosolapov.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Alexander-Kosolapov"
+		"link" => BASE_URL."artists/Alexander_Kosolapov"
 		),
 	$meshberg = array(
 		"title" => "Lev Meshberg",
 		"img" => IMG_PATH."Lev Meshberg.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Lev-Meshberg"
+		"link" => BASE_URL."artists/Lev_Meshberg"
 		),
 	$vassiliev = array(
 		"title" => "Oleg Vassiliev",
 		"img" => IMG_PATH."Oleg Vassiliev.jpg",
 		
-		"link" => BASE_URL."artists/artist.php?artist=Oleg-Vassiliev"
+		"link" => BASE_URL."artists/Oleg_Vassiliev"
 		),
 	$rapoport = array(
 		"title" => "Alek Rapoport",
 		"img" => IMG_PATH."Alek Rapoport.jpg",
-		"link" => BASE_URL."artists/artist.php?artist=Alek-Rapoport"
+		"link" => BASE_URL."artists/Alek_Rapoport"
 		),
 	$nemukhin = array(
 		"title" => "Vladimir Nemukhin",
 		"img" => IMG_PATH."Vladimir Nemukhin.jpg",
-		"link" => BASE_URL."artists/artist.php?artist=Vladimir-Nemukhin"
+		"link" => BASE_URL."artists/Vladimir_Nemukhin"
 		),
 	$rabine = array(
 		"title" => "Oscar Rabine",
 		"img" => IMG_PATH."Oscar Rabine.jpg",
-		"link" => BASE_URL."artists/artist.php?artist=Oscar-Rabine"
+		"link" => BASE_URL."artists/Oscar_Rabine"
 		),
 	);
 ?>
